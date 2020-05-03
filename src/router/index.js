bc51140bd3ced5849387a07b4496beda7c317177
Iamtanzim <tanzim11@gmail.com>
2020-05-03 20:31:44 +0600

    Global components needs to get fixed could not load loadash

---
+++ src/router/index.js
@@ -0,0 +1,77 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Home from "../views/Home.vue";
+// import About from "../views/About.vue";
+// import User from "../views/User.vue";
+import EventList from "../views/EventList.vue";
+import EventCreate from "../views/EventCreate.vue";
+import EventShow from "../views/EventShow.vue";
+import EventCard from "../components/EventCard.vue";
+
+
+
+
+
+
+Vue.use(VueRouter);
+
+
+
+const router = new VueRouter({
+  //If you give the history mode it no longer shows hash in route
+  mode:"history",
+  routes:[
+    {
+      path: "/",
+      name: "event-list",
+      component: EventList
+
+    },
+    {
+      path: "/event-create",
+      name: "event-create",
+      component: EventCreate
+
+    },
+    {
+      path: "/event/:id",
+      name: "event-show",
+      component: EventShow,
+      props:true
+
+    },
+    {
+      path: "/event-card",
+      name: "event-card",
+      component: EventCard,
+     
+
+    },
+    {
+      path: "/event/create",
+      name: "event-create",
+      component: EventCreate
+
+    },
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: Home
+
+    // },
+    // {
+    //   path: "/about-us",
+    //   name: "about",
+    //   component: About
+
+    // },
+    // {
+    //   path: "/user/:username",
+    //   name: "user",
+    //   component: User,
+    //   props: true,
+    // }
+  ]
+});
+
+export default router;
